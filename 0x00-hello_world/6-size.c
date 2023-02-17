1db07ab1cbd7f5020a891365a587142fc2138802
Daisy-dawn <agboella20@gmail.com>
2023-02-16 22:52:15 -0800

    Task666

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 
 int main(void)
 {
-printf("size of a char: %lu byte(s)", sizeof(char));
-printf("size of an int: %lu byte(s)", sizeof(int));
-printf("size of a long int: %lu byte(s)", sizeof(long int));
-printf("size of a long long int: %lu byte(s)", sizeof(long long int));
-printf("size of a float: %lu byte(s)", sizeof(float));
+printf("size of a char: %zu byte(s)", sizeof(char));
+printf("size of an int: %zu byte(s)", sizeof(int));
+printf("size of a long int: %zu byte(s)", sizeof(long int));
+printf("size of a long long int: %zu byte(s)", sizeof(long long int));
+printf("size of a float: %zu byte(s)", sizeof(float));
 return (0);
 }
