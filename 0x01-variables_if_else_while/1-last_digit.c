4221674f812ef3a44de3517d8b8dc55d5ca9ff89
Daisy-dawn <agboella20@gmail.com>
2023-02-17 15:32:17 -0800

    Task1 git commit -m Task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,11 @@
  */
 int main(void)
 {
-	int n;
+	int n,
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10
+	last = n % 10;
 	if (last > 5)
 	{
 	printf("Last digit of %i is %i and is greater than 5\n", n, last);
