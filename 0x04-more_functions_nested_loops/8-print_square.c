3c310d119358360248ed0aecfd7a3122d8c06f7f
Daisy-dawn <agboella20@gmail.com>
2023-02-23 06:14:51 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - printing a square size
+ * @size: size of the square
+ *
+ * Return: void
+ */
+
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
