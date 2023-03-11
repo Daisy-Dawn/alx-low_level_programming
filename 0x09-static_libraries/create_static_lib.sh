6637c5d5c53929daacdc74e2c8c3629892f67958
Daisy-dawn <agboella20@gmail.com>
2023-03-10 17:14:25 -0800

    Modified Task 0

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-# a script that creates a static library
-# called liball.a from all the .c files
-# that are in the current directory
-
-gcc -Wall -pedantic - Werror -Wextra -c *.c
-ar rc liball.a *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
 ranlib liball.a
